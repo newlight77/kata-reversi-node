798535c6c3d3f0e62e26988dc71e921fac819c20
Kong To <newlight77@gmail.com>
2022-05-10 20:24:27 +0200

    TDD : write basic board game second test, introducing concept of board as model

--- test/board.game.spec.ts
+++ test/board.game.spec.ts
@@ -1,7 +1,26 @@
+type Row = string[];
+export type Board = Row[];
+
+const initialState: Board = [
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", "B", "W", ".", ".", "."],
+    [".", ".", ".", "W", "B", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "."],
+];
 
 export class BoardGame {
+    state: Board;
+    
+    constructor(state = initialState) {
+        this.state = state;
+    }
+
     display(): string {
-      return "";
+       return this.state.toString();
     }
 }
 
@@ -11,9 +30,41 @@ describe("Display board", function () {
         const board = new BoardGame();
 
         // Act 
-        const result = board.display();
+        const displayedBoard = board.display();
 
         // Assert
-        expect(result).toEqual(expect.any(String));
+        expect(displayedBoard).toEqual(expect.any(String));
+    });
+
+
+    test("Should return an empty board", function () {
+        // Arrange
+        const initialBoard: Board = [
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", "B", "W", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+        ];
+        const expectedBoard: Board = [
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", "B", "W", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+            [".", ".", ".", ".", ".", ".", ".", "."],
+        ];
+        const board = new BoardGame(initialBoard);
+
+        // Act 
+        const displayedBoard = board.display();
+
+        // Assert
+        expect(displayedBoard).toEqual(expectedBoard.toString());
     });
 });
